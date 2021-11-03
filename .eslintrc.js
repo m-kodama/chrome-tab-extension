b62732b9cf03e95e88b29eccbf74374e16d7211e
m-kodama <tc003ssr@gmail.com>
2021-11-03 23:26:42 +0900

    feat: 保存時に自動でフォーマットされるよう設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,17 +5,29 @@ module.exports = {
     webextensions: true,
   },
   extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
+    'plugin:vue/essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
   ],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'all',
+        printWidth: 80,
+        endOfLine: 'lf',
+        tabWidth: 2,
+        semi: true,
+        bracketSpacing: true,
+      },
+    ],
   },
 };
