04bb29c0512877632d07310b81219d67540a5647
m-kodama <tc003ssr@gmail.com>
2021-11-04 21:52:16 +0900

    feat: App.vueにstyleを追加

---
+++ src/components/Icon.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <span>{{ msg }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+div {
+  min-height: 36px;
+}
+</style>
