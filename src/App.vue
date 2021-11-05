79f93c81da66b31542b7da2402a73cc415687d47
m-kodama <tc003ssr@gmail.com>
2021-11-05 23:23:54 +0900

    feat: bodyのstyleを追加

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,18 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
+$surface-color: #121212;
+$text-color: #ffffff;
+body {
+  width: 800px;
+  height: 600px;
+  margin: 0;
+  background-color: $surface-color;
+  color: $text-color;
+  font-family: 'Avenir', 'Helvetica Neue', 'Helvetica', 'Arial',
+    'ヒラギノ角ゴ Pro W3', 'Hiragino Kaku Gothic Pro', 'メイリオ', 'Meiryo',
+    '游ゴシック', 'Yu Gothic', 'ＭＳ Ｐゴシック', sans-serif;
+}
 #app {
   min-height: 36px;
 }
