92c0454131c0350acc475f6dd1adeba4c88b76db
m-kodama <tc003ssr@gmail.com>
2021-11-14 23:42:43 +0900

    feat: hot reload対応

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,11 @@
 /* eslint-disable no-undef */
 module.exports = {
   publicPath: './',
+  configureWebpack: (config) => {
+    config.devtool = 'inline-source-map';
+  },
   chainWebpack: (config) => {
     config.resolve.alias.set('vue', '@vue/compat');
-
     config.module
       .rule('vue')
       .use('vue-loader')
